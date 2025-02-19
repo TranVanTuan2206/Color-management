f314f844e07bd8bd819a56ea7418df9d721fed2d
Tuan Tran <tuanvan8163@gmail.com>
2025-02-19 20:07:36 +0700

    fix issues

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 if (getData()) {
   display(getData());
-  document.querySelector("#addSource").innerHTML = "Change Source";
+  document.querySelector("#addSource").innerHTML = "Change  Source";
   document.querySelector("#checkDuplicate").style.display = "inline-block";
   document.querySelector("#resetFilter").style.display = "inline-block";
 } else {
-  document.querySelector("#addSource").innerHTML = "Add Source";
+  document.querySelector("#addSource").innerHTML = "Add  Source";
   document.querySelector("#checkDuplicate").style.display = "none";
   document.querySelector("#resetFilter").style.display = "none";
 }
@@ -17,7 +17,7 @@ function addSource() {
     return;
   }
   if (!text && !getData()) {
-    window.alert("please add source!!");
+    window.alert("please  add  source!!");
     return;
   }
   const arrayStr = text.split("{");
@@ -38,7 +38,7 @@ function addSource() {
     summaryTheme.push({
       name: general.name,
       light: general.color,
-      dark: "Both as one",
+      dark: "Both  as  one",
     });
   });
 
@@ -55,7 +55,7 @@ function addSource() {
       summaryTheme.push({
         name: light.name,
         light: light.color,
-        dark: "No Color",
+        dark: "No  Color",
       });
     }
   });
@@ -63,14 +63,14 @@ function addSource() {
   darkArray.map((dark) => {
     summaryTheme.push({
       name: dark.name,
-      light: "No Color",
+      light: "No  Color",
       dark: dark.color,
     });
   });
   setData(summaryTheme);
   let data = getData();
   display(data);
-  document.querySelector("#addSource").innerHTML = "Change Source";
+  document.querySelector("#addSource").innerHTML = "Change  Source";
   document.querySelector("#checkDuplicate").style.display = "inline-block";
   document.querySelector("#resetFilter").style.display = "inline-block";
 }
@@ -79,10 +79,10 @@ function reFormatColorArray(colorArr) {
   colorArr = colorArr
     .split(";")
     .filter((e) => e)
-    .map((e) => e.replace("  ", "").replaceAll("\n", ""));
+    .map((e) => e.replace("    ", "").replaceAll("\n", ""));
   let result = [];
   colorArr.map((color) => {
-    const colorArrEle = color.split(": ");
+    const colorArrEle = color.split(": ");
     result.push({
       name: colorArrEle[0],
       color: colorArrEle[1],
@@ -93,7 +93,7 @@ function reFormatColorArray(colorArr) {
 
 function display(data) {
   data = data.map((e) => {
-    return `<tr><td><button onclick="copy(event,'${e.name}')">Copy</button>${e.name}</td><td>${e.light}</td><td>${e.dark}</td></tr>`;
+    return `<tr><td><button  onclick="copy(event,'${e.name}')">Copy</button>${e.name}</td><td>${e.light}</td><td>${e.dark}</td></tr>`;
   });
   const str = data.join("");
   document.querySelector("tbody").innerHTML = str;
@@ -103,7 +103,7 @@ function copy(event, value) {
   event.target.innerHTML = "Copied";
   event.target.style.background = "#b3ebb3";
   if (value.includes("--")) {
-    navigator.clipboard.writeText("var(" + value + ")");
+    navigator.clipboard.writeText("var(" + value.replaceAll(' ', '') + ")");
     return;
   }
   navigator.clipboard.writeText(value);
@@ -142,6 +142,7 @@ function filterColor() {
 
 function getData() {
   data = localStorage.getItem("data");
+  console.log(JSON.parse(data));
   return JSON.parse(data);
 }
 function setData(data) {
@@ -150,11 +151,11 @@ function setData(data) {
 
 function subColor(data) {
   return (data = data.map((e, i, arr) => {
-    if (e.color.includes("var(")) {
+    if (e.color?.includes("var(")) {
       const name = e.color.replace("var(", "").replace(")", "");
       const found = arr.find((e) => e.name === name);
       if (!found) {
-        e.color += "---Not found";
+        e.color += "---Not  found";
         return e;
       }
       e.color += "---" + found.color;
@@ -199,7 +200,7 @@ function displayForCheckDuplicate(data) {
   data = data.map((e) => {
     let subStr = e.map(
       (k) =>
-        `<tr><td><button onclick="copy(event,'${k.name}')">Copy</button>${k.name}</td><td>${k.light}</td><td>${k.dark}</td></tr>`
+        `<tr><td><button onclick="copy(event,'${k.name}')">Copy</button>${k.name}</td><td>${k.light}</td><td>${k.dark}</td></tr>`
     );
     subStr += `<tr><td>===</td><td>===</td><td>===</td></tr>`;
     return subStr.replaceAll("</tr>,", "</tr>");
